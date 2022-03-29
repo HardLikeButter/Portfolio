0053575e83065ce63521f456038ad221b2607821
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-03-28 22:53:46 -0400

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link"); 
 
 
+
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
 })
@@ -10,4 +11,21 @@ navLinks.forEach(link => {
     link.addEventListener("click", () => {
         document.body.classList.remove("nav-open");
     })
+})
+
+
+// Reveal hidden Content Buttons
+const btnOne = document.querySelector(".revealBtn__one");
+const btnTwo = document.querySelector(".revealBtn__two");
+const answerOne = document.querySelector(".answer-one");
+const answerTwo = document.querySelector(".answer-two")
+
+
+btnOne.addEventListener("click", function () {
+    answerOne.classList.toggle("show-answer");
+});
+
+
+btnTwo.addEventListener("click", () => {
+    answerTwo.classList.toggle("show-answer");
 })
